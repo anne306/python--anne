79726aa480a88babdf7c3fa89dfd60c5f7d28db6
eMobilis <you@example.com>
2023-02-13 10:05:14 +0300

    my forever project

---
+++ oops/house.py
@@ -0,0 +1,12 @@
+class House1():
+    doors = 4
+    windows = 16
+    kitchen = 1
+    dinning = 2
+
+house1obj = House1()
+print(house1obj.doors, house1obj.dinning, house1obj.windows, house1obj.kitchen)
+
+
+
+#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
